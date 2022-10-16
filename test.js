8227b1340dc924e6c057a28c461a68324fd177db
Link <h26281660@gmail.com>
2022-10-16 11:44:43 +0800

    test ajax

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
-import cheerio from 'cheerio'
+// import cheerio from 'cheerio'
 
-axios.get('https://wdaweb.github.io/').then(({ data }) => {
-  const $ = cheerio.load(data)
-  console.log($('#go .col-lg-3.col-md-6').eq(0).find('h4').text())
+// axios.get('https://wdaweb.github.io/').then(({ data }) => {
+//   const $ = cheerio.load(data)
+//   console.log($('#go .col-lg-3.col-md-6').eq(0).find('h4').text())
+// })
+
+axios.get(encodeURI('https://iplay.sa.gov.tw/api/GymSearchAllList?$format=application/json;odata.metadata=none&Keyword=排球場')).then(({ data }) => {
+  console.log(data)
 })
