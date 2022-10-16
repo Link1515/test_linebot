0767ffb314a20b51f42646e42729e7e5b633f8e5
Link <h26281660@gmail.com>
2022-10-16 17:49:43 +0800

    add express

--- test.js
+++ test.js
@@ -1,15 +1,18 @@
+import express from 'express'
 import axios from 'axios'
-// import cheerio from 'cheerio'
 
-// axios.get('https://wdaweb.github.io/').then(({ data }) => {
-//   const $ = cheerio.load(data)
-//   console.log($('#go .col-lg-3.col-md-6').eq(0).find('h4').text())
-// })
+const app = express()
 
 axios.defaults.timeout = 40000
 
-axios.get(encodeURI('https://iplay.sa.gov.tw/api/GymSearchAllList?$format=application/json;odata.metadata=none&Keyword=排球場')).then(({ data }) => {
-  console.log(data)
-}).catch(err => {
-  console.log(err)
+app.get('/ajax', async (req, res) => {
+  try {
+    const { data } = await axios.get(encodeURI('https://iplay.sa.gov.tw/api/GymSearchAllList?$format=application/json;odata.metadata=none&Keyword=排球場'))
+    return res.send(data)
+  } catch (error) {
+    console.log(error)
+    return res.send('error')
+  }
 })
+
+app.listen(process.env.PORT || 3000)
