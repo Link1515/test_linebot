66d9f2adffa7dc5476c1ed4ad259137f237b22c0
Link <h26281660@gmail.com>
2022-10-16 18:37:58 +0800

    console data

--- test.js
+++ test.js
@@ -1,20 +1,22 @@
-import express from 'express'
-import axios from 'axios'
+// import express from 'express'
+// import axios from 'axios'
 import data from './data.js'
 
-const app = express()
+// const app = express()
 
-axios.defaults.timeout = 40000
+// axios.defaults.timeout = 40000
 
-app.get('/ajax', (req, res) => {
-  try {
-    return res.send({ data })
-  } catch (error) {
-    console.log(error)
-    return res.send('error')
-  }
-})
+// app.get('/ajax', (req, res) => {
+//   try {
+//     return res.send({ data })
+//   } catch (error) {
+//     console.log(error)
+//     return res.send('error')
+//   }
+// })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('server running...')
-})
+// app.listen(process.env.PORT || 3000, () => {
+//   console.log('server running...')
+// })
+
+console.log(data)
