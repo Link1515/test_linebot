dc706e31a4a6272acbd6451512fa45a579a958e5
Link <h26281660@gmail.com>
2022-10-16 17:59:19 +0800

    log data length

--- test.js
+++ test.js
@@ -8,11 +8,13 @@ axios.defaults.timeout = 40000
 app.get('/ajax', async (req, res) => {
   try {
     const { data } = await axios.get(encodeURI('https://iplay.sa.gov.tw/api/GymSearchAllList?$format=application/json;odata.metadata=none&Keyword=排球場'))
-    return res.send(data)
+    return res.send({ data: data.length })
   } catch (error) {
     console.log(error)
     return res.send('error')
   }
 })
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000, () => {
+  console.log('server running...')
+})
